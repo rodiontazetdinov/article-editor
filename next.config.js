ec8003fa52acfedcd1084b479dadee41ae99d5c6
rodiontazetdinov <rodiontazetdinov@yandex.ru>
2024-12-23 19:57:37 +0300

    подготовка к деплою

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/article-editor',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
